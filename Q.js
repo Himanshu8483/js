c496a78cd0fcf8a40647332055ce0a903f606119
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2024-12-08 17:04:49 +0530

    all

---
+++ Q.js
@@ -0,0 +1,96 @@
+function start(){ 
+    // 1.
+    // let age=prompt("Enter Your Age");
+    // age>=18 ? alert("Adult") : alert("Not Adult");
+
+    // 2.
+    // let ans1= confirm ("Are you get Certificate")
+    // let ans2= confirm ("Are you completed your Course")
+    // if(ans1 && ans2){
+    // alert("Completed")
+    // }
+    // else {
+    // alert("Not Completed")
+    // }
+    // 3.
+    // let price= prompt("Enter Price");
+    // let discount= confirm ("Are you have a discount coupon");
+    // if (discount==true) {
+    //     alert("10% Discount");
+    // }
+    // else {
+    //     alert("sorry");
+    // }
+    // 4.
+    // let score= prompt("Enter Percentage");
+    // if(score>=33 && score<=100)
+    // {
+    //     alert("pass");
+    // }
+    // else {
+    //     alert ("fail");
+    // }
+    // 5.
+    // let num= prompt("Enter num to check even or odd");
+    // if(num%2==0)
+    // {
+    //     alert("even");
+    // }
+    // else {
+    //     alert ("odd");
+    // }
+    // 6.
+    // let num= confirm("Are you sure to delete");
+    // if(num==true)
+    // {
+    //     alert("deleted");
+    // }
+    // else {
+    //     alert ("cencelled");
+    // }
+    // 7.
+    // let num= prompt("Enter Number");
+    // if(num>=0)
+    // {
+    //     alert("positive");
+    // }
+    // else {
+    //     alert ("negative");
+    // }
+    // 8.
+    // let num = 10;
+    // let i=1 ;
+    // while(i<=num){
+    //     console.log(i);
+    //     ++i;
+    // }
+    // 9.
+    // let num;
+    // do{
+    //     num = prompt("Enter Number");
+    // }
+    // while(num<=10);
+    // alert ("Thankyou");
+
+    // 10.
+    // let day = prompt("enter num to print day")
+    // switch(parseInt(day)){
+    //     case 1 :alert("Monday")
+    //     break;
+    //     case 2 :alert("Tuesday")
+    //     break;
+    //     case 3 :alert("Wednesday")
+    //     break;
+    //     case 4 :alert("Thursday")
+    //     break;
+    //     case 5 :alert("Friday")
+    //     break;
+    //     case 6 :alert("Saturday")
+    //     break;
+    //     case 7 :alert("Sunday")
+    //     break;
+    //     default: alert ("Invalid")
+    //     break;
+    // }
+}
+start();
