15e7131aa38e721960cd8be2f3b483a2c2ffecc9
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2024-12-11 12:43:41 +0530

    all

--- array.js
+++ array.js
@@ -49,8 +49,8 @@ function okk(){
 // okk();
 
 function start(){
-    // document.querySelector
-    let show = document.getElementById("text")
+    let show = document.querySelector("#text")
+    // let show = document.getElementById("text")
     // show.textContent="It's Coming"
     show.innerHTML= "<h1> Its Come </h1>"
     // show.style.color="red";
