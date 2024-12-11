2f9709cec31a3f3ea1d90969eb81ce7f4dc7cf7c
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2024-12-11 12:24:35 +0530

    all

---
+++ array.js
@@ -0,0 +1,45 @@
+function okk(){
+
+    let fruits = ["Apple", "Mango" , "Banana", "Grapes"] ;
+
+    // let ans=fruits.length;  // 4
+    // document.write(ans);
+
+    // pop() remove last element from array
+    // fruits.pop();
+    // fruits.pop();
+    // fruits.pop();
+    // document.write(fruits);
+    
+    // shift() remove starting element from array
+//     fruits.shift();
+//     fruits.shift();
+//     fruits.shift();
+//     document.write(fruits);
+// }
+
+    // push() add extra element at last from array
+    // fruits.push("Papaya", "Pineapple" ,"Orange");
+    // document.write(fruits);
+
+    // unshift() add extra element at start from array
+    // let ans= fruits.push("Papaya", "Pineapple" ,"Orange")
+    // fruits.unshift("Papaya", "Pineapple" ,"Orange");
+    // document.write(ans);
+    // document.write(fruits);
+
+    
+    let arr1=[4,3,6,1,2,9]
+    // let ans= arr1.sort()
+    // document.write(ans)         // assending order
+    // document.write(arr1)        //change the original array
+
+    // let ans1= arr1.reverse()     //reverse order
+    // document.write(ans1)
+
+    // let ans1= arr1.sort().reverse()    // dessending order
+    // document.write(ans1)
+
+    
+}
+// okk();
