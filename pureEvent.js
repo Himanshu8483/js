3f29ad4d2dfc82f07c37346dab299549aba49cc3
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2024-12-21 12:43:30 +0530

    all

--- pureEvent.js
+++ pureEvent.js
@@ -14,9 +14,25 @@ btn.addEventListener("mouseenter", ()=>{      // for hover
     //     console.log(e.key);
     // })
 
-    // input live print on page 
-    let inp=document.querySelector("#inp")
-    inp.addEventListener("input", ()=>{
-    let show=document.querySelector("#show")
-    show.innerHTML=inp.value
+    // // input live print on page 
+    // let inp=document.querySelector("#inp")
+    // inp.addEventListener("input", ()=>{
+    // let show=document.querySelector("#show")
+    // show.innerHTML=inp.value
+    // })
+
+    let inp1=document.querySelector("#red")
+    inp1.addEventListener("click", ()=>{
+        let show=document.querySelector(".box")
+        show.style.backgroundColor="red";
+    })
+    let inp2=document.querySelector("#yellow")
+    inp2.addEventListener("click", ()=>{
+        let show=document.querySelector(".box")
+        show.style.backgroundColor="yellow";
+    })
+    let inp3=document.querySelector("#green")
+    inp3.addEventListener("click", ()=>{
+        let show=document.querySelector(".box")
+        show.style.backgroundColor="green";
     })
