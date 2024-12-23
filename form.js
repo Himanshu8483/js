b386dbddf2b320f2144d0b4325121e25b79baa9a
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2024-12-23 23:48:03 +0530

    all

--- form.js
+++ form.js
@@ -1,17 +1,20 @@
 
 let submitt=()=>{
+      // Fetching input values
     let name=document.querySelector("#inpname").value;
     let email=document.querySelector("#inpemail").value;
     let number=document.querySelector("#inpnum").value;
     let pass=document.querySelector("#inppass").value;
     let cpass=document.querySelector("#inpcpass").value;
 
+        // Fetching error message elements
     let errname=document.querySelector("#errorname");
     let erremail=document.querySelector("#erroremail");
     let errnumber=document.querySelector("#errornum");
     let errpass=document.querySelector("#errorpass");
     let errcpass=document.querySelector("#errorcpass");
 
+      // Name validation
     if(name===""){
         let inpnum=document.querySelector("#inpname")
         inpnum.placeholder="Not filled";
@@ -22,17 +25,20 @@ let submitt=()=>{
         return false;
     }
 
-    else if(email===""){
-        erremail.innerHTML="Please enter your email";
-        return false;
-    }
-
+      // Number validation
     else if (isNaN(number) || number.length !== 10) {
         errnumber.innerHTML = "Please enter a valid 10-digit number";
         errnumber.style.color = "red";
         return false;
     }
 
+     // Email validation
+     else if(email===""){
+        erremail.innerHTML="Please enter your email";
+        return false;
+    }
+
+       // Password validation
     else if(pass === "") {
         errpass.innerHTML = "Password is required";
         return false;
