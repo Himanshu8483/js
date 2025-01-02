c9d25eecdd684e90850696b91f36efa75441e66c
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-01-02 12:48:13 +0530

    all

--- new/date.js
+++ new/date.js
@@ -1,22 +1,30 @@
 
-let time= new Date();
-console.log(time.toLocaleString());
+// let time= new Date();
+// console.log(time.toLocaleString());
 
-let time1=time.getFullYear()    // year
-let time2=time.getMonth()    // month
-let time3=time.getDay()     // day
-let time4=time.getHours()   // hour
-let time5=time.getMinutes()     // minutes
-let time6=time.getSeconds()     // second
-let time7=time.getMilliseconds()
-console.log(time7);
+// let time1=time.getFullYear()    // year
+// let time2=time.getMonth()    // month
+// let time3=time.getDay()     // day
+// let time4=time.getHours()   // hour
+// let time5=time.getMinutes()     // minutes
+// let time6=time.getSeconds()     // second
+// let time7=time.getMilliseconds()    // millisecond
+// console.log(time7);
 
-let timee = ()=> { 
+// let timee = ()=> { 
 
-    let show = document.querySelector("#target"); 
+//     let show = document.querySelector("#target"); 
+//     let time= new Date(); 
+// // show.innerHTML= time.getFullYear();
+// show.innerHTML= time.getDay();
+// let arr=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday" ,"Friday", "Saturday"]
+// show.innerHTML= arr[time.getDay()]      // day name print
+// }
+
+let age = ()=> { 
+    let show = document.querySelector("#text").value;
     let time= new Date(); 
-// show.innerHTML= time.getFullYear();
-show.innerHTML= time.getDay();
-let arr=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday" ,"Friday", "Saturday"]
-show.innerHTML= arr[time.getDay()]      // day name print
+    let result= time.getFullYear()-show;
+    let show1=document.querySelector("#show")
+    show1.innerHTML=result;
 }
